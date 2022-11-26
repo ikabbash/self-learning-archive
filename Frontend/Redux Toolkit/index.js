5c48bdf0b95e0bdb07bc683af54e3ba93c4921ab
Valdus <i.kabbash@protonmail.ch>
2022-11-26 11:39:10 +0200

    Redux 2

--- Frontend/Redux Toolkit/index.js
+++ Frontend/Redux Toolkit/index.js
@@ -1,14 +1,23 @@
 const redux = require('redux'); // now to implement the store
 const createStore = redux.createStore
+const bindActionCreators = redux.bindActionCreators
 
 const CAKE_ORDERED = 'CAKE_ORDERED' // how Redux defines states
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
 
 // action creator, returns the object
 function orderCake() {
     // action, object with type property
     return {
         type: CAKE_ORDERED,
-        quantity: 1,
+        payload: 1,
+    }
+}
+
+function restockCake(qty = 1) {
+    return {
+        type: CAKE_RESTOCKED,
+        payload: qty,
     }
 }
 
@@ -29,6 +38,11 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 numOfCakes: state.numOfCakes -1,
             }
+        case CAKE_RESTOCKED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes + action.payload,
+            }
         default:
             return state
     }
@@ -39,10 +53,19 @@ const store = createStore(reducer)
 console.log('Initial state:', store.getState())
 // unsubscribe is the final step
 const unsubscribe = store.subscribe (() => console.log('Update state: ', store.getState()))
+
 // action creator
-store.dispatch(orderCake())
-store.dispatch(orderCake())
-store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(restockCake(3))
+
+// second argument is what we want to bind it to
+const actions = bindActionCreators({ orderCake, restockCake}, store.dispatch)
+actions.orderCake()
+actions.orderCake()
+actions.orderCake()
+actions.restockCake(3)
 
 // if you are to dispatch after unsubscribe changes won't be visible
 unsubscribe();
