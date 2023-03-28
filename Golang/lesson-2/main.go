518e7728149a3b18a2264f14fc38e4abd16e37e1
Valdus <i.kabbash@protonmail.ch>
2023-03-29 00:06:52 +0200

    Golang 1

---
+++ Golang/lesson-2/main.go
@@ -0,0 +1,16 @@
+// to indicate the entry point of the program
+// package main indicates that this is an executable program
+// the main package must contain a main func and this is where code is executed
+package main
+
+// you can create your own package name it anything else other than main
+// then can be imported
+
+// fmt is a package, for formatting functions
+// you can use the functions, types, and variables defined in that package
+import "fmt"
+
+// the entry point of the application, one and only one main
+func main() {
+	fmt.Println("Hello, V4LDUS")
+}
