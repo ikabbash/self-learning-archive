1e80ba013e36e0280cc610758b96a100ace237bd
ikabbash <i.kabbash21@gmail.com>
2025-08-14 12:20:24 +0300

    update: Testing built-in tools

--- Elixir/example4/lib/example4.ex
+++ Elixir/example4/lib/example4.ex
@@ -2,18 +2,18 @@ defmodule Example4 do
   use Application
 
   def start(_type, _args) do
-      Example4.main()
-      Supervisor.start_link([], strategy: :one_for_one)
+    Example4.main()
+    Supervisor.start_link([], strategy: :one_for_one)
   end
 
   def main do
-      name = "Hamada"
-      status = Enum.random([:gold, :silver])
+    name = "Hamada"
+    status = Enum.random([:gold, :silver])
 
-      if status === :gold do
-        IO.puts("Welcome, #{name}.")
-      else
-        IO.puts("Leave.")
-      end
+    if status === :gold do
+      IO.puts("Welcome, #{name}.")
+    else
+      IO.puts("Leave.")
+    end
   end
 end
