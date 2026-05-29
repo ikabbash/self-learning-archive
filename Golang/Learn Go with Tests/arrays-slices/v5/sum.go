b739e7e31f5d386088389d3421f681685538556b
ikabbash <i.kabbash21@gmail.com>
2026-05-29 21:32:48 +0300

    update: Go TDD slices sum tails

---
+++ Golang/Learn Go with Tests/arrays-slices/v5/sum.go
@@ -0,0 +1,35 @@
+package main
+
+// requirement is to change SumAll to SumAllTails, where it will calculate the totals of the "tails" of each slice
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		// in case of empty slice
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
